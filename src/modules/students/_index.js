6f68e034c88c87f6afd4731de1a91bd9e490816e
Sardor <henkook-baron@mail.ru>
2023-07-10 19:22:31 +0500

    create resolver showStudent

--- src/modules/students/_index.js
+++ src/modules/students/_index.js
@@ -2,6 +2,7 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { pubsub } from '../../graphql/pubsub.js';
 import { listStudents } from './list-students.js';
+import { showStudent } from './show-student.js';
 
 const typeDefs = readFileSync(join(process.cwd(), 'src', 'modules', 'students', '_schema.gql'), 'utf8');
 
@@ -9,6 +10,9 @@ const resolvers = {
   Query: {
     students: () => {
       return listStudents();
+    },
+    student: (_, args) => {
+      return showStudent({ id: args.id });
     }
   }
 };
