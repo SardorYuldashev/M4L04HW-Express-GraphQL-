ec818ca57baafb3327330e630f0621b38aa9de55
Sardor <henkook-baron@mail.ru>
2023-07-10 11:20:23 +0500

    knexfile configured

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,73 @@
-// Update with your config settings.
+import config from './src/shared/config/index.js';
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+export default {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user:     config.db.user,
+      password: config.db.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds'
     }
   },
 
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user:     config.db.user,
+      password: config.db.password
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds'
     }
   },
 
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user:     config.db.user,
+      password: config.db.password
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds'
     }
   }
 
