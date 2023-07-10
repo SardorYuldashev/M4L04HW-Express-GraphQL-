17f1a6bfb391ea76ce3d8c5534263e45d820a3d5
Sardor <henkook-baron@mail.ru>
2023-07-10 17:36:08 +0500

    create showCategory resolver

--- src/modules/categories/_index.js
+++ src/modules/categories/_index.js
@@ -2,13 +2,17 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { pubsub } from '../../graphql/pubsub.js';
 import { listCategories } from './list-categories.js';
+import { showCategory } from './show-category.js';
 
 const typeDefs = readFileSync(join(process.cwd(), 'src', 'modules', 'categories', '_schema.gql'), 'utf8');
 
-const resolvers ={
+const resolvers = {
   Query: {
     categories: () => {
       return listCategories();
+    },
+    category: (_, args) => {
+      return showCategory({ id: args.id });
     }
   }
 };
--- src/modules/comments/show-comment.js
+++ src/modules/comments/show-comment.js
@@ -1,12 +1,12 @@
 import db from '../../db/index.js';
-import { NotFoundError } from '../../shared/errors/index.js'
+import { NotFoundError } from '../../shared/errors/index.js';
 
 export const showComment = async ({ id }) => {
-  const post = await db('comments').where({ id }).first();
+  const comment = await db('comments').where({ id }).first();
 
-  if (!post) {
+  if (!comment) {
     throw new NotFoundError('Comment not found');
   };
 
-  return post;
+  return comment;
 };
