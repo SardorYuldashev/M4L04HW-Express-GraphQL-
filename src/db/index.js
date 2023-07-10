380a1cc779f615359b97b81ab9e87549d1fe5396
Sardor <henkook-baron@mail.ru>
2023-07-10 11:28:07 +0500

    create src/db/index.js file

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import knex from 'knex';
+import config from '../shared/config/index.js';
+
+/**
+ * @type {knex.Knex}
+ */
+const db = knex({
+  client: 'postgresql',
+  connection: {
+    host: config.db.host,
+    port: config.db.port,
+    database: config.db.name,
+    user: config.db.user,
+    password: config.db.password
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+});
+
+export default db;
