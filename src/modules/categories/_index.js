0336d4a005836f1b43f7c468dbc5bf950ac31cca
Sardor <henkook-baron@mail.ru>
2023-07-10 17:53:41 +0500

    realized updateCategory resolver

--- src/modules/categories/_index.js
+++ src/modules/categories/_index.js
@@ -4,6 +4,7 @@ import { pubsub } from '../../graphql/pubsub.js';
 import { listCategories } from './list-categories.js';
 import { showCategory } from './show-category.js';
 import { addCategory } from './add-category.js';
+import { editCategory } from './edit-category.js';
 
 const typeDefs = readFileSync(join(process.cwd(), 'src', 'modules', 'categories', '_schema.gql'), 'utf8');
 
@@ -20,9 +21,12 @@ const resolvers = {
     createCategory: (_, args) => {
       const result = addCategory(args.input);
 
-      pubsub.publish('CATEGORY_CREATED', {categoryCreated: result});
+      pubsub.publish('CATEGORY_CREATED', { categoryCreated: result });
 
       return result;
+    },
+    updateCategory: (_, args) => {
+      return editCategory({ id: args.id, ...args.input });
     }
   },
   Subscription: {
