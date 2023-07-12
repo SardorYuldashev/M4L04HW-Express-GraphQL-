ac7267b391be21c5bcaeffe8df5292267310ac30
Sardor <henkook-baron@mail.ru>
2023-07-12 16:56:00 +0500

    bug fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import http from 'http';
 import express from 'express';
 import cors from 'cors';
 import { buildGraphQLServer } from './graphql/index.js';
+import { expressMiddleware } from '@apollo/server/express4';
 import config from './shared/config/index.js';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -10,10 +12,25 @@ const httpServer = http.createServer(app);
 app.use(express.json());
 app.use(cors());
 
-const { server, serverMiddleware } = buildGraphQLServer(httpServer);
+const server = buildGraphQLServer(httpServer);
 
 await server.start();
-app.use('/gql', serverMiddleware);
+app.use(
+  '/gql',
+  expressMiddleware(server, {
+    context: ({ req }) => {
+      const token = req.headers.authorization;
+
+      if (token) {
+        const decoded = jwt.verify(token, config.jwt.secret);
+
+        return { user: decoded.user };
+      };
+
+      return { user: {} };
+    }
+  })
+);
 
 const PORT = config.PORT || 8080;
 httpServer.listen(PORT, () => {
